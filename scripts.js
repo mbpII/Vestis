b4092afb8206c8d835c08e6ca4428c7a4ae0a517
mbpII <mattparksii@outlook.com>
2024-04-07 20:26:02 -0400

    adjusting styling, added navigation

--- scripts.js
+++ scripts.js
@@ -2,58 +2,71 @@ let sites = [
   {
     name: "Pro-Club",
     url: "https://shopproclub.com/ ",
+    imgurl: "/img/proclub.png",
     description: "the essentials tees, socks, etc",
   },
   {
     name: "Seventh Earth",
     url: "https://7thearthstudios.com/ ",
+    imgurl: "/img/blank.png",
     description: "Blank hoodies, sweats, and crewneck sweaters  ",
   },
   {
     name: "MoojiMooji",
     url: "https://moojimoojius.com/",
-    description: "Colorful sweats",
+    imgurl: "/img/mooji.png",
+    description:
+      "Colorful sweats Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
-    name: "Radar Club",
-    url: "http://Radarclothingclub.com/ ",
-    description: "Graphic tees, and hoodies",
+    name: "Momotaro Jeans",
+    url: "https://momotaro-jeans.com/ ",
+    imgurl: "/img/jeans.png",
+    description: "A Japanese denim website I always lose",
   },
   {
     name: "Whitesville Loopwheel Tees",
     url: "https://redcastheritage.com/collections/whitesville/ ",
+    imgurl: "/img/whitesville.png",
     description: "The T-shirt from The Bear",
   },
   {
     name: "Waffle sweats",
     url: "https://www.cottonnovas.com/products/heavyweight-knitted-cotton-waffle-sweatpant",
+    imgurl: "/img/wafflesweats.png",
     description:
       "So interesting sweatpaints I found that ind never seen before",
   },
   {
-    name: "Barrier",
+    name: "Barriers",
     url: "https://barriersworldwide.com/",
+    imgurl: "/img/barriers.png",
     description:
       "A brand heightlights Black historical figures and icons through clothing",
   },
   {
-    name: "Momotaro Jeans",
-    url: "https://momotaro-jeans.com/ ",
-    description: "A Japanese denim website I always lose",
+    name: "Radar Club",
+    url: "http://Radarclothingclub.com/ ",
+    imgurl: "/img/radar.webp",
+    description: "Graphic tees, and hoodies",
   },
   {
     name: "Zara",
     url: "https://www.zara.com/us/en/origins-collection-l4661.html?v1=2297670&regionGroupId=13 ",
-    description: "Zara but the good stuff",
+    imgurl: "/img/zara.png",
+    description: "Zara but the good stuff Lorem ipsum dolor sit am",
   },
   {
     name: "Uniqlo",
     url: "https://www.uniqlo.com/us/en/contents/feature/masterpiece/ ",
-    description: "Uniqlo but the good stuff",
+    imgurl: "/img/",
+    description:
+      "Uniqlo but where they hide the good stuff, mostly high quility essentials",
   },
   {
     name: "California-arts",
     url: "https://california-arts.com/",
+    imgurl: "/img/caliart.png",
     description: "A minimalist brand focused on craftmenship and quility",
   },
 ];
@@ -70,13 +83,18 @@ let smartCasual = sites.slice(8, 11);
 function showCards(cardsArray) {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = ""; // Clear existing content
+  navigation();
 
   cardsArray.forEach((site) => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
           <h2>${site.name}</h2>
-          <p>${site.description}</p>
+          <img
+          src="${site.imgurl}"
+          alt=""
+        />
+          <p style="padding: 20px;">${site.description}</p>
       `;
     card.onclick = function () {
       window.location.href = site.url;
@@ -85,21 +103,26 @@ function showCards(cardsArray) {
   });
 }
 
-// Call the function with the 'basic' array to show cards on load
-document.addEventListener("DOMContentLoaded", function () {
-  showCards(sites);
-});
-
-let cardSelection = document.getElementById("selection");
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
+function navigation() {
+  const footer = document.querySelector("footer");
+  footer.style.display = "block";
 }
 
-function removeLastCard() {
-  sites.pop(); // Remove last item in names array
-  showCards(); // Call showCards again to refresh
+function setCardSelection(buttonValue) {
+  let cardSelection = "basic";
+  cardSelection = buttonValue;
+  console.log(cardSelection);
+  if (cardSelection === "basic") {
+    showCards(basic);
+  } else if (cardSelection === "cool") {
+    showCards(cool);
+  } else if (cardSelection === "smartCasual") {
+    showCards(smartCasual);
+  }
 }
+
+function home() {
+  window.location.href = "index.html";
+}
+// make remove cards button
+//make navigation show when card visible
